8735c77097ede353db46ac2dbe90beac3dcebb81
Shen-Han <56202340+Shen-Han@users.noreply.github.com>
2020-02-21 23:39:07 -0500

    Add files via upload

---
+++ Models/Cartitem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopCart1.Models
+{
+    public class Cartitem
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public Cartitem(Product product, int quantity)
+        {
+            this.Product = product;
+            this.Quantity = quantity;
+        }
+    }
+}
